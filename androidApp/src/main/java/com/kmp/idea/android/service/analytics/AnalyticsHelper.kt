7634cf048f45e3fc91c3d5c58159d465a92777d5
Darren Lambert <73501755+Dardev12@users.noreply.github.com>
2025-02-08 18:38:48 -0500

    Setup Firebase Android
    todo :
    - UI Component
    - UI Page
    - Fix error Klint & moko

--- androidApp/src/main/java/com/kmp/idea/android/service/analytics/AnalyticsHelper.kt
+++ androidApp/src/main/java/com/kmp/idea/android/service/analytics/AnalyticsHelper.kt
@@ -1,3 +1,22 @@
 package com.kmp.idea.android.service.analytics
 
-class AnalyticsHelper
+import android.content.Context
+import android.os.Bundle
+import com.google.firebase.analytics.FirebaseAnalytics
+
+class AnalyticsHelper(private val context: Context) {
+
+    private val firebaseAnalytics = FirebaseAnalytics.getInstance(context)
+
+    fun logEvent(eventName: String, params: Bundle? = null) {
+        firebaseAnalytics.logEvent(eventName, params)
+    }
+
+    fun setUserId(userId: String) {
+        firebaseAnalytics.setUserId(userId)
+    }
+
+    fun setUserProperty(value: String?, property: String) {
+        firebaseAnalytics.setUserProperty(property, value)
+    }
+}
